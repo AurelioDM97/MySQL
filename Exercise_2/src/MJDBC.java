ce71ea02c57e8caf3b11a7cdb524ce3e6aee501a
AurelioDM97 <126873779+AurelioDM97@users.noreply.github.com>
2023-06-30 16:32:16 +0200

    add screen
    added screen

--- Exercise_2/src/MJDBC.java
+++ Exercise_2/src/MJDBC.java
@@ -1,2 +1,19 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class MJDBC {
+        public static void main(String[] args) {
+            try {
+                Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3303/newdb", "developer", "developer");
+                Statement statement = connection.createStatement();
+                ResultSet resultSet = statement.executeQuery("SELECT * FROM students");
+                while (resultSet.next()) {
+                    System.out.println(resultSet.getString("first_name"));
+                    System.out.println(resultSet.getString("last_name"))
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
 }
