dc50138e28c6d241a882bd740b3b5f412cc102a5
AurelioDM97 <126873779+AurelioDM97@users.noreply.github.com>
2023-06-30 16:40:14 +0200

    m
    m

--- src/Exercise_2/MJDBC.java
+++ src/Exercise_2/MJDBC.java
@@ -1,16 +1,18 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 
 public class MJDBC {
         public static void main(String[] args) {
             try {
-                Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3303/newdb", "developer", "developer");
+                Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/newdb", "root", "developer");
                 Statement statement = connection.createStatement();
                 ResultSet resultSet = statement.executeQuery("SELECT * FROM students");
                 while (resultSet.next()) {
                     System.out.println(resultSet.getString("first_name"));
-                    System.out.println(resultSet.getString("last_name"))
+                    System.out.println(resultSet.getString("last_name"));
                 }
             } catch (Exception e) {
                 e.printStackTrace();
