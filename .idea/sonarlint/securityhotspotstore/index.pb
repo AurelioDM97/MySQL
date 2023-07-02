1e11d96cfe3e18a8243cf611ba8e37f9a6efa612
AurelioDM97 <126873779+AurelioDM97@users.noreply.github.com>
2023-07-02 18:14:19 +0200

    FIXXED 2 EXERCISE

--- .idea/sonarlint/securityhotspotstore/index.pb
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -1,7 +1,5 @@
 
 9
 	MySql.iml,3\d\3df70a7fea3101a58a9e5ad11352c7cbd199dfef
-I
-src/Exercise_2/MJDBC.java,f\c\fc7d55f3b4224db005d92b3cd7536b4f7d1f01b1
 H
-src/Exercise_3/Main.java,f\5\f5e054a5f3213ac2243b94faf43a7e9d7101bfff
+src/Exercise_4/Main.java,a\2\a2d71045ee0b3074cee3f396232989dd2e9f03bd
--- src/Exercise_2/MJDBC.java
+++ src/Exercise_2/MJDBC.java
@@ -5,6 +5,21 @@ import java.sql.*;
 
 public class MJDBC {
     public static void main(String[] args) {
+
+        String tableCreation = "CREATE TABLE students (" +
+                "student_id INT NOT NULL AUTO_INCREMENT,"+
+                "first_name varchar(30)," +
+                "last_name varchar(30)," +
+                "CONSTRAINT student_pk PRIMARY KEY (student_id))";
+
+        String fillStudentTable = "INSERT INTO students (first_name, last_name)" +
+                "VALUES ('Paolo', 'Albergadori'),"+
+                "('Pio', 'Fratta'),"+
+                "('Giovanni', 'Malocchio'),"+
+                "('Tullio', 'Colirio')";
+
+
+
         try (Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/newdb", "root", "aureliodimarco97!");
              Statement statement = connection.createStatement();
              ResultSet resultSet = statement.executeQuery("SELECT * FROM students")) {
